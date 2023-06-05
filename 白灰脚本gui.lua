916eea281d61900eea1cb630f3498a1753885eaf
XiaoYunCN <130466576+XiaoYunCN@users.noreply.github.com>
2023-06-05 20:49:07 +0800

    Update 白灰脚本gui.lua

--- 白灰脚本gui.lua
+++ 白灰脚本gui.lua
@@ -23,13 +23,17 @@ if game.PlaceId == 3101667897 then --极速传奇
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87.lua"))()
 
+elseif game.PlaceId == 155615604 then
+	
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E7%9B%91%E6%8E%A7.lua",true))()
+	
 elseif game.PlaceId == 3956818381 then --忍者传奇
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E5%BF%8D%E8%80%85%E4%BC%A0%E5%A5%87.lua"))()
 
 elseif game.PlaceId == 1318971886 then --Break in (Stay)
     loadstring(game:HttpGet("https://pastebin.com/raw/28Z4ExEn", true))()
 	
-elseif game.PlaceId == 3623096087 then --力量传奇
+力量传奇
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87%E6%9C%80%E5%BC%BA.lua"))()
 
