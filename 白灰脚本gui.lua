53c0d7b36cdecf83e4f7eab64dca68c4a5bff056
XiaoYunCN <130466576+XiaoYunCN@users.noreply.github.com>
2023-06-10 17:58:29 +0800

    更新 白灰脚本gui.lua

--- 白灰脚本gui.lua
+++ 白灰脚本gui.lua
@@ -33,6 +33,9 @@ elseif game.PlaceId == 3956818381 then --忍者传奇
 elseif game.PlaceId == 1318971886 then --Break in (Stay)
     loadstring(game:HttpGet("https://pastebin.com/raw/28Z4ExEn", true))()
 	
+elseif game.PlaceId == 189707 then -
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E8%87%AA%E7%84%B6%E7%81%BE%E5%AE%B3%E7%94%9F%E5%AD%98%E6%B8%B8%E6%88%8F.lua", true))()
+
 elseif game.PlaceId == 1318971886 then --力量传奇
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87%E6%9C%80%E5%BC%BA.lua"))()
