4e12fb1a18ede6fe2f73511f81d57853d29f3c60
XiaoYunCN <130466576+XiaoYunCN@users.noreply.github.com>
2023-06-15 22:17:13 +0800

    Update 白灰脚本gui.lua

--- 白灰脚本gui.lua
+++ 白灰脚本gui.lua
@@ -1,4 +1,4 @@
-game.Players.LocalPlayer:Kick("公告：白灰已停止，在白灰脚本群有新白灰脚本最新版)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E7%99%BD%E7%81%B0%E8%84%9A%E6%9C%ACgui.lua"))()
 
 --[[
 raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E7%99%BD%E7%81%B0%E8%84%9A%E6%9C%AC.lua"))()
