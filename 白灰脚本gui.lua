7cd875211da7ee8c0fc0c7749c492b3edea87127
XiaoYunCN <130466576+XiaoYunCN@users.noreply.github.com>
2023-06-20 21:58:58 +0800

    Update 白灰脚本gui.lua

--- 白灰脚本gui.lua
+++ 白灰脚本gui.lua
@@ -1,6 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E7%99%BD%E7%81%B0%E8%84%9A%E6%9C%ACgui.lua"))()
-
---[[
-raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E7%99%BD%E7%81%B0%E8%84%9A%E6%9C%AC.lua"))()
-
-]]
+_G.Clouduilib = "白灰脚作者小云，加载出十几秒"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/White-ash-script/main/%E7%99%BD%E7%81%B0%E8%84%9A%E6%9C%ACbeta.lua", true))()
